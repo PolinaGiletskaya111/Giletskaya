0f7417e8b6ddac53a0c1117640f08aae5a4ace19
PolinaGiletskaya111 <149323430+PolinaGiletskaya111@users.noreply.github.com>
2023-11-12 23:28:01 +0300

    Add files via upload

---
+++ Лабораторная работа №3/ЛР3.3.py
@@ -0,0 +1,62 @@
+# TODO  Напишите функцию count_letters
+def count_letters(text):
+    letters_dict = {}
+    for i in text.lower():
+        if i.isalpha() and i not in letters_dict:
+            letters_dict[i] = text.lower().count(i)
+    return letters_dict
+
+
+def calculate_frequency(letters_dict):
+    frequency_letters = {}
+    all_count = sum(letters_dict.values())
+    for letter in letters_dict.keys():
+        frequency_letters[letter] = format(round(letters_dict[letter] / all_count,2), ".2f")
+    return frequency_letters
+
+
+
+
+
+# TODO Напишите функцию calculate_frequency
+
+
+main_str = """
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+Там на неведомых дорожках
+Следы невиданных зверей;
+Избушка там на курьих ножках
+Стоит без окон, без дверей;
+Там лес и дол видений полны;
+Там о заре прихлынут волны
+На брег песчаный и пустой,
+И тридцать витязей прекрасных
+Чредой из вод выходят ясных,
+И с ними дядька их морской;
+Там королевич мимоходом
+Пленяет грозного царя;
+Там в облаках перед народом
+Через леса, через моря
+Колдун несёт богатыря;
+В темнице там царевна тужит,
+А бурый волк ей верно служит;
+Там ступа с Бабою Ягой
+Идёт, бредёт сама собой,
+Там царь Кащей над златом чахнет;
+Там русский дух… там Русью пахнет!
+И там я был, и мёд я пил;
+У моря видел дуб зелёный;
+Под ним сидел, и кот учёный
+Свои мне сказки говорил.
+"""
+
+# TODO Распечатайте в столбик букву и её частоту в тексте
+for letters, frequency in calculate_frequency(count_letters(main_str)).items():
+    print(f"{letters}: {frequency}")
