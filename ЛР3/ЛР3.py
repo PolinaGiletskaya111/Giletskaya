b841170582788ed417e2855bfc3b667b1141da17
PolinaGiletskaya111 <149323430+PolinaGiletskaya111@users.noreply.github.com>
2024-02-14 17:41:25 +0300

    Лабораторная работа №3 (Модуль №2)

---
+++ ЛР3/ЛР3.py
@@ -0,0 +1,46 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def name(self):
+        return self._name
+
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name, author, pages: int):
+        super().__init__(name, author)
+        if not isinstance(pages, int):
+            raise TypeError("Колиечство страниц должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Колиечство страниц должно быть положительным числом")
+        self.pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name, author, duration: float):
+        super().__init__(name, author)
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность должна быть типа float")
+        if duration <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self.duration = duration
+
+    def __str__(self):
+        return f"Электронная книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
